a953cc0f9066e6120afe5e0300ef7522751c1e6a
Tom Maszk <tmaszk@pobox.com>
2016-12-07 08:32:05 -0500

    Increase history size

---
+++ shell/config/history.sh
@@ -0,0 +1,28 @@
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# store multiline commands as one command
+shopt -s cmdhist
+
+# See http://unix.stackexchange.com/a/48116
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=100000
+HISTFILESIZE=$HISTSIZE
+
+history() {
+  _bash_history_sync
+  builtin history "$@"
+}
+
+_bash_history_sync() {
+  builtin history -a
+  HISTFILESIZE=$HISTSIZE
+  builtin history -c
+  builtin history -r
+}
+
