6caf1b62d71041dc92a430186d6beceb7c62bff1
tmaszk <tom.maszk@blackbaud.com>
2017-10-16 11:14:24 -0400

    Add md and other misc aliases

---
+++ shell/aliases/misc_aliases.sh
@@ -0,0 +1,23 @@
+alias md='mkdir'
+
+## from https://www.cyberciti.biz/tips/bash-aliases-mac-centos-linux-unix.html
+
+## get rid of command not found ##
+alias cd..='cd ..'
+
+## a quick way to get out of current directory ##
+alias ..='cd ..'
+alias ...='cd ../../../'
+alias ....='cd ../../../../'
+alias .....='cd ../../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../..'
+
+## Colorize the grep command output for ease of use (good for log files)##
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# install  colordiff package :)
+alias diff='colordiff'
+
