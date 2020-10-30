2768c51f5f785f232aeb433e509758d701898475
Tom Maszk <tmaszk@pobox.com>
2020-10-30 13:48:43 -0400

    Expand change directory aliases

--- shell/aliases/misc_aliases.sh
+++ shell/aliases/misc_aliases.sh
@@ -2,16 +2,16 @@ alias md='mkdir'
 
 ## from https://www.cyberciti.biz/tips/bash-aliases-mac-centos-linux-unix.html
 
-## get rid of command not found ##
-alias cd..='cd ..'
-
 ## a quick way to get out of current directory ##
-alias ..='cd ..'
+alias ..='cd ../../'
 alias ...='cd ../../../'
 alias ....='cd ../../../../'
-alias .....='cd ../../../../'
+alias .....='cd ../../../../../'
+alias .1='cd ../'
+alias .2='cd ../../'
+alias .3='cd ../../../'
 alias .4='cd ../../../../'
-alias .5='cd ../../../../..'
+alias .5='cd ../../../../../'
 
 ## Colorize the grep command output for ease of use (good for log files)##
 alias grep='grep --color=auto'
