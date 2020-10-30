7ae320dce23217d828b0bc6a234e64c576d164f3
Tom Maszk <tmaszk@Toms-MBP-2018.attlocal.net>
2020-10-30 11:34:59 -0400

    Improve gup alias to fetch

--- shell/aliases/git_aliases.sh
+++ shell/aliases/git_aliases.sh
@@ -157,9 +157,10 @@ alias gtv='git tag | sort -V'
 
 alias gunignore='git update-index --no-assume-unchanged'
 alias gunwip='git log -n 1 | grep -q -c "\-\-wip\-\-" && git reset HEAD~1'
-alias gup='git pull --rebase'
+alias gup='git pull --rebase --autostash && gfa'
 alias gupv='git pull --rebase -v'
 alias glum='git pull upstream master'
 
 alias gwch='git whatchanged -p --abbrev-commit --pretty=medium'
 alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit -m "--wip--"'
+
